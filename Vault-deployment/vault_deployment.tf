b3da6886025272f3352995944a72d4c73c51f399
Murodbey <abdullayevmurod@gmail.com>
2019-04-19 16:26:01 -0500

    updated

--- Vault-deployment/vault_deployment.tf
+++ Vault-deployment/vault_deployment.tf
@@ -17,14 +17,17 @@ resource "kubernetes_persistent_volume_claim" "vault_pvc" {
   }
 }
 resource "kubernetes_deployment" "vault" {
-  # depends_on = ["kubernetes_secret.vault_secret"]
+  depends_on = ["kubernetes_secret.vault_secret"]
+
   metadata {
     name      = "vault"
     namespace = "tools"
+
     labels {
       app = "vault-deployment"
     }
   }
+
   spec {
     replicas = 1
 
@@ -44,23 +47,27 @@ resource "kubernetes_deployment" "vault" {
       spec {
         volume {
           name = "vault-pvc"
+
           persistent_volume_claim {
             claim_name = "vault-pvc"
           }
         }
+
         container {
           name  = "vault"
           image = "vault"
+
           port {
             container_port = 8200
             protocol       = "TCP"
           }
+
           security_context {
-            # allow_privilege_escalation = false
             capabilities {
               add = ["IPC_LOCK"]
             }
           }
+
           env {
             name = "VAULT_DEV_ROOT_TOKEN_ID"
 
@@ -71,28 +78,7 @@ resource "kubernetes_deployment" "vault" {
               }
             }
           }
-            # privileged = true
-            # run_as_non_root = true
-         
-            # privileged = false
-            # run_as_non_root = true
-            # run_as_user = 10001
-          }
-          # env {
-          # #   name  = "vault-token"
-          # #   value = "${var.vault_token}"
-          # # }
-          #     secret_key_ref {
-          #       name = "vault-secret"
-          #       key  = "token"
-          #     }
-          #   }
-          # }
-          # env_from {
-          #   secret_ref {
-          #     name = "vualt-secret"
-          #   }
-          # }
+
           volume_mount {
             name       = "vault-pvc"
             mount_path = "/var/run"
@@ -101,6 +87,8 @@ resource "kubernetes_deployment" "vault" {
       }
     }
   }
+}
+
 resource "kubernetes_service" "vault_service" {
   # depends_on = ["kubernetes_secret.vault_secret"]
   metadata {
