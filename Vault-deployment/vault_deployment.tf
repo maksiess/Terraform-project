d04e59080f60d8048014c8a0c61b4c64bc26f47c
Murodbey <abdullayevmurod@gmail.com>
2019-04-19 13:19:00 -0500

    updated

--- Vault-deployment/vault_deployment.tf
+++ Vault-deployment/vault_deployment.tf
@@ -68,12 +68,18 @@ resource "kubernetes_deployment" "vault" {
             # run_as_user = 10001
           }
           env {
-            name  = "VAULT_TOKEN_ID"
-            value_from {
-              secret_key_ref {
-                name = "vault-secret"
-                key  = "token"
-              }
+            name  = "vault-token"
+            value = "${var.vault_token}"
+          }
+          #     secret_key_ref {
+          #       name = "vault-secret"
+          #       key  = "token"
+          #     }
+          #   }
+          # }
+          env_from {
+            secret_ref {
+              name = "vualt-secret"
             }
           }
           volume_mount {
