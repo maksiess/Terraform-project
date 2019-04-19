8f8376c475ecf1dc3b18cf6f3b8fc8d007a7d0c8
Murodbey <abdullayevmurod@gmail.com>
2019-04-19 13:40:40 -0500

    updated

--- Vault-deployment/secrets.tf
+++ Vault-deployment/secrets.tf
@@ -1,10 +1,10 @@
-resource "kubernetes_secret" "vault_secret" {
-  metadata {
-    name      = "vault-secret"
-    namespace = "tools"
-  }
-  data {
-    token = "${var.vault_token}"
-    }
-  type = "Opaque"
-}
+# resource "kubernetes_secret" "vault_secret" {
+#   metadata {
+#     name      = "vault-secret"
+#     namespace = "tools"
+#   }
+#   data {
+#     token = "${var.vault_token}"
+#     }
+#   type = "Opaque"
+# }
