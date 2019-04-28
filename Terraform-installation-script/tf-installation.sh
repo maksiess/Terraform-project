55ed2f84aaf0d6eef497a60349cb3f157a1c6d56
Murodbey <abdullayevmurod@gmail.com>
2019-04-28 17:10:41 -0500

    tf-script

--- Terraform-installation-script/tf-installation.sh
+++ Terraform-installation-script/tf-installation.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo yum install wget -y
+sudo apt-get install wget -y
 wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
 unzip terraform_0.11.11_linux_amd64.zip
 sudo mv terraform /bin/
 terraform --version
 terraform --help
-sudo yum install git -y
+sudo apt-get install git -y
+sudo yum install git -y 
