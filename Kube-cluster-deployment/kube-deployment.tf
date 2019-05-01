84e9d7050f6a7e8cfa94927f2412db203e8e937e
Murodbey <abdullayevmurod@gmail.com>
2019-05-01 17:14:20 -0500

    updated

--- Kube-cluster-deployment/kube-deployment.tf
+++ Kube-cluster-deployment/kube-deployment.tf
@@ -35,18 +35,18 @@ resource "google_container_cluster" "gke-cluster" {
     # subnetwork          = "us-central1"
     location            = "us-central1"
     min_master_version  = "1.11.8-gke.6"
-    # initial_node_count  = "1"
+    initial_node_count  = "1"
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "snoop-dogg-pool"
   location   = "us-central1"
   cluster    = "${google_container_cluster.gke-cluster.name}"
-  node_count = 2
+  node_count = 1
 
   node_config {
     preemptible  = true
-    machine_type = "n1-standard-1"
+    machine_type = "n1-standard-2"
 
   }
 }
