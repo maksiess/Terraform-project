036c364d56b0def17bc981df07bd2f355eabcd74
Murodbey <abdullayevmurod@gmail.com>
2019-05-01 13:23:23 -0500

    kube deployment created

---
+++ Kube-cluster-deployment/kube-deployment.tf
@@ -0,0 +1,25 @@
+module "my-cluster" {
+    name                = "snoop-dogg"
+    network             = "default"
+    subnetwork          = "us-central1"
+    zone                = "us-central1-a"
+    kube_version        = "1.11.8-gke.6"
+    initial_node_count  = "3"
+    machine_type        = "n1-standard-2"
+}
+
+# resource "google_container_cluster" "gke-cluster" {
+#   name                     = "${var.name}"
+#   location                 = "${var.zone}"
+#   remove_default_node_pool = true
+#   initial_node_count       = 1
+#   min_master_version       = "${var.kube_version}"
+#   enable_legacy_abac       = false
+#   master_auth {
+#     username = ""
+#     password = ""
+#     client_certificate_config {
+#       issue_client_certificate = false
+#     }
+#   }
+# }
