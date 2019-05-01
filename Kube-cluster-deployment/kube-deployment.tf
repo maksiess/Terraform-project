9521236b2bed99c2d41a211fc83c97107112b40f
Murodbey <abdullayevmurod@gmail.com>
2019-05-01 17:51:16 -0500

    updated

--- Kube-cluster-deployment/kube-deployment.tf
+++ Kube-cluster-deployment/kube-deployment.tf
@@ -35,6 +35,7 @@ resource "google_container_cluster" "gke-cluster" {
     # subnetwork          = "us-central1"
     location            = "us-central1"
     min_master_version  = "1.11.8-gke.6"
+    remove_default_node_pool = true
     initial_node_count  = "1"
 }
 
@@ -47,6 +48,5 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   node_config {
     preemptible  = true
     machine_type = "n1-standard-2"
-
   }
 }
