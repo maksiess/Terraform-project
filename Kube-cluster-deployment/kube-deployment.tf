b8aef8f1047d85a768f7e410d875057c9b16f917
Murodbey <abdullayevmurod@gmail.com>
2019-05-01 19:05:31 -0500

    updated

--- Kube-cluster-deployment/kube-deployment.tf
+++ Kube-cluster-deployment/kube-deployment.tf
@@ -7,35 +7,29 @@ resource "google_container_cluster" "gke-cluster" {
     initial_node_count  = 1
     remove_default_node_pool = true
 
-    node_config {
+    node_pool {
+      name = "default-pool"
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "snoop-dogg-pool"
+  location   = "us-central1"
+  cluster    = "${google_container_cluster.gke-cluster.name}"
+  node_count = 1
+
+  node_config {
     preemptible  = true
-    machine_type = "n1-standard-1"
+    machine_type = "n1-standard-2"
+
+    metadata {
+      disable-legacy-endpoints = "true"
+    }
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
-    }
+  }
 }
-
-# resource "google_container_node_pool" "primary_preemptible_nodes" {
-#   name       = "snoop-dogg-pool"
-#   location   = "us-central1"
-#   cluster    = "${google_container_cluster.gke-cluster.name}"
-#   node_count = 1
-
-#   node_config {
-#     preemptible  = true
-#     machine_type = "n1-standard-2"
-
-#     metadata {
-#       disable-legacy-endpoints = "true"
-#     }
-
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/logging.write",
-#       "https://www.googleapis.com/auth/monitoring",
-#     ]
-#   }
-# }
