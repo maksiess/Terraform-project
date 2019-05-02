310ec60cd0175bc6957d79068bb684eb06bd1438
Murodbey <abdullayevmurod@gmail.com>
2019-05-02 18:40:59 -0500

    updated

--- Service-account-gcp/outputs.tf
+++ Service-account-gcp/outputs.tf
@@ -24,10 +24,13 @@ output "private_key" {
 
 output "decoded_private_key" {
     value = "${base64decode(google_service_account_key.mykey.private_key)}"
-}
-
-provisioner "remote-exec" {
     inline = [
       "terraform output -json decoded_private_key >> private_key.json",
     ]
-  }
+}
+
+# provisioner "remote-exec" {
+#     inline = [
+#       "terraform output -json decoded_private_key >> private_key.json",
+#     ]
+#   }
