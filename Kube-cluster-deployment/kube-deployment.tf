31ee0f14bcf2384e2d40da97c3719a8cc6f7e263
Murodbey <abdullayevmurod@gmail.com>
2019-05-01 19:59:32 -0500

    update

--- Kube-cluster-deployment/kube-deployment.tf
+++ Kube-cluster-deployment/kube-deployment.tf
@@ -1,48 +1,48 @@
 resource "google_container_cluster" "gke-cluster" {
     name                = "snoop-dogg"
     network             = "default"
-    # subnetwork          = "us-central1"
-    location            = "us-central1"
+    subnetwork          = "default"
+    zone                = "us-central1-a"
     min_master_version  = "1.11.8-gke.6"
-    initial_node_count  = "1"
-    remove_default_node_pool = true
+    initial_node_count  = "2"
+    # default_node_pool   = false
     node_config {
       # image_type   = "${var.node_image_type}"
-      machine_type = "n1-standard-1"
+      machine_type = "n1-standard-2"
       # disk_size_gb = "${var.node_disk_size_gb}"
 
     # The set of Google API scopes
     # The following scopes are necessary to ensure the correct functioning of the cluster
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+    # oauth_scopes = [
+    #   "https://www.googleapis.com/auth/compute",
+    #   "https://www.googleapis.com/auth/devstorage.read_only",
+    #   "https://www.googleapis.com/auth/logging.write",
+    #   "https://www.googleapis.com/auth/monitoring",
+    # ]
 
     # Tags can used to identify targets in firewall rules
     # tags = ["${var.name}-cluster", "nodes"]
   }
 }
 
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "snoop-dogg-pool"
-  location   = "us-central1"
-  cluster    = "${google_container_cluster.gke-cluster.name}"
-  node_count = 1
+# resource "google_container_node_pool" "primary_preemptible_nodes" {
+#   name       = "snoop-dogg-pool"
+#   location   = "us-central1"
+#   cluster    = "${google_container_cluster.gke-cluster.name}"
+#   node_count = 1
 
-  node_config {
-    preemptible  = true
-    machine_type = "n1-standard-2"
+#   node_config {
+#     preemptible  = true
+#     machine_type = "n1-standard-2"
 
-    metadata {
-      disable-legacy-endpoints = "true"
-    }
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
-}
+#     metadata {
+#       disable-legacy-endpoints = "true"
+#     }
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/compute",
+#       "https://www.googleapis.com/auth/devstorage.read_only",
+#       "https://www.googleapis.com/auth/logging.write",
+#       "https://www.googleapis.com/auth/monitoring",
+#     ]
+#   }
+# }
