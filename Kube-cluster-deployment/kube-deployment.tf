01e68458d20b0543e2d978b51a29de2049ff6c94
Murodbey <abdullayevmurod@gmail.com>
2019-05-01 19:23:23 -0500

    updated

--- Kube-cluster-deployment/kube-deployment.tf
+++ Kube-cluster-deployment/kube-deployment.tf
@@ -4,7 +4,7 @@ resource "google_container_cluster" "gke-cluster" {
     # subnetwork          = "us-central1"
     location            = "us-central1"
     min_master_version  = "1.11.8-gke.6"
-    initial_node_count  = 1
+    initial_node_count  = "1"
     # remove_default_node_pool = true
     node_config {
       # image_type   = "${var.node_image_type}"
