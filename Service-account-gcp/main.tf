cfd27f41570ec972568cb6697b6796a8ad57449c
Murodbey <abdullayevmurod@gmail.com>
2019-05-05 15:05:52 -0500

    updated

--- Service-account-gcp/main.tf
+++ Service-account-gcp/main.tf
@@ -16,12 +16,12 @@ resource "google_service_account_key" "mykey" {
 #   valid_before        = "2014-10-02T15:01:23.045123456Z"  (The key can be used before this timestamp. A timestamp in RFC3339 UTC "Zulu" format, accurate to nanoseconds.) 
 }
 
-resource "google_project_iam_member" "shermat" {
-    depends_on        = ["google_service_account.murodbey"]
-    # count             =  "${length(var.roles)}"
-    role              =  "${var.roles}"
-    member            =  "serviceAccount:${google_service_account.murodbey.email}"
-}
+# resource "google_project_iam_member" "shermat" {
+#     depends_on        = ["google_service_account.murodbey"]
+#     # count             =  "${length(var.roles)}"
+#     role              =  "${var.roles}"
+#     member            =  "serviceAccount:${google_service_account.murodbey.email}"
+# }
 
 resource "local_file" "serviceAccountKey" {
     content     = "${base64decode(google_service_account_key.mykey.private_key)}"
