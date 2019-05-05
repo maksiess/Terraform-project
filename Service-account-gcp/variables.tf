099ecfd8f5351613b0d3275be5b1cb4937e3a222
Murodbey <abdullayevmurod@gmail.com>
2019-05-05 15:01:27 -0500

    updated

--- Service-account-gcp/variables.tf
+++ Service-account-gcp/variables.tf
@@ -5,7 +5,7 @@ variable "display_name" {
     description = "The display name for the service account. Can be updated without creating a new resource."
 }
 variable "roles" {
-    type        = "list"
+    # type        = "list"
     description = "The roles that will be granted to the account."
     default     = ["roles.serviceAccount.admin"]
 }
