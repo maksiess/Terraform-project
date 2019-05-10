2c03797f4c2a966b15646e724667a14430c5f5f0
Murodbey <abdullayevmurod@gmail.com>
2019-05-10 12:57:47 -0500

    updated

--- Kube-cluster-deployment/kube-deployment.tf
+++ Kube-cluster-deployment/kube-deployment.tf
@@ -17,13 +17,13 @@ resource "google_container_cluster" "gke-cluster" {
     node_config {
       # image_type   = "${var.node_image_type}"
       machine_type      = "n1-standard-2"
-      # disk_size_gb = "${var.node_disk_size_gb}"
-    resource_limits {
-      resource_type = "cpu"
-      minimum = "4"
-      resource_type = "memory"
-      minimum = "10"
-      }
+      disk_size_gb      = "10Gb"
+    # resource_limits {
+    #   resource_type = "cpu"
+    #   minimum = "4"
+    #   resource_type = "memory"
+    #   minimum = "10"
+    #   }
 
     # The set of Google API scopes
     # The following scopes are necessary to ensure the correct functioning of the cluster
