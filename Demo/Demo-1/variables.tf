6723eeb71f1bbc9fbef95dbacc74b7ffa74e72f7
root <root@ip-172-31-47-138.eu-west-1.compute.internal>
2019-12-25 17:56:38 +0000

    demo

--- Demo/Demo-1/variables.tf
+++ Demo/Demo-1/variables.tf
@@ -1,7 +1,9 @@
 variable "AWS_ACCESS_KEY" {
+  default = ""
 }
 
 variable "AWS_SECRET_KEY" {
+  default = ""
 }
 
 variable "AWS_REGION" {
@@ -9,10 +11,9 @@ variable "AWS_REGION" {
 }
 
 variable "AMIS" {
-  type = map(string)
   default = {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    eu-west-1 = "ami-01f14919ba412de34"
   }
 }
