b370feeda1a776038af681f64ace934a03ba9699
Murodbey <abdullayevmurod@gmail.com>
2019-12-26 12:03:11 -0600

    updated

--- Demo/Demo-2/instance.tf
+++ Demo/Demo-2/instance.tf
@@ -6,7 +6,7 @@ resource "aws_key_pair" "mykey" {
 resource "aws_instance" "example" {
   ami           = "${var.AMIS[var.AWS_REGION]}"
   instance_type = "t2.micro"
-  key_name      = "mykey"
+  key_name      = "aws_key_pair.mykey.key_name"
 
   provisioner "file" {
     source      = "script.sh"
