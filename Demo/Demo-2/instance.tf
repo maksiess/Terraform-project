3580f35e03cb5551548fabb06fc9ebcd91b3d2c0
Murodbey <abdullayevmurod@gmail.com>
2019-12-26 12:14:27 -0600

    updated

--- Demo/Demo-2/instance.tf
+++ Demo/Demo-2/instance.tf
@@ -22,6 +22,6 @@ resource "aws_instance" "example" {
     #host        = coalesce(self.public_ip, self.private_ip)
     type        = "ssh"
     user        = "${var.INSTANCE_USERNAME}"
-    private_key = "${var.PATH_TO_PRIVATE_KEY}"
+    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
   }
 }
