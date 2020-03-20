7044eb6ddb994d13f089e2ceafdd2168725f8540
Murodbey <abdullayevmurod@gmail.com>
2020-03-20 01:58:32 +0000

    updated

--- Azure/3-tier-application/backend.tf
+++ Azure/3-tier-application/backend.tf
@@ -5,7 +5,7 @@ terraform {
     resource_group_name  = "dev"
     storage_account_name = "devmurodbey"
     container_name       = "devcontainer"
-    access_key           = "b8jEIEt68kC5NiGAxKwt9gawspmpPTzRkieVlXed6f8LhsjpXgtpKrsUSEA2hgfHUTAdsp74SC83cH4B0XUFdQ=="
+    access_key           = ""
     key                  = "dev_terraform.tfstate"
   }
 }
