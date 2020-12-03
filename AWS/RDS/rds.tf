7f8437ec789159dcd13e5efca545bd4cad297245
Murodbey <abdullayevmurod@gmail.com>
2020-12-03 11:08:48 -0600

    av zone updated

--- AWS/RDS/rds.tf
+++ AWS/RDS/rds.tf
@@ -9,6 +9,7 @@ resource "aws_db_instance" "rds" {
 	username = "postgress"
 	password = "foobarbaz"
 	parameter_group_name = "default:postgres-12"
+    availability_zone = "us-east-2"
 	publicly_accessible = true
 	db_subnet_group_name = "${aws_db_subnet_group.db.name}"
 	skip_final_snapshot = true #used to delete the repo in the future without this you cant delete. There are bugs reported 
