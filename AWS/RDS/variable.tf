6ec069e55596e40174d9860b85bd467fd2b98425
Murodbey <abdullayevmurod@gmail.com>
2020-12-03 11:32:23 -0600

    fixed

--- AWS/RDS/variable.tf
+++ AWS/RDS/variable.tf
@@ -4,7 +4,7 @@ variable "password" {}
 variable "name" {}
 variable "engine_version" {}
 variable "engine" {}
-variable "db-instance_type" {}
+variable "db_instance_type" {}
 variable "storage_type" {}
 variable "storage_cap" {}
 variable "identifier" {}
