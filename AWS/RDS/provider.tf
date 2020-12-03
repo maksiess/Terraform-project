d3a131f947c03c4cec07c8f28a6a8cdc06fe4c62
Murodbey <abdullayevmurod@gmail.com>
2020-12-03 11:10:11 -0600

    provider file added

---
+++ AWS/RDS/provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+region = "us-east-2"
+}
+
+terraform {
+    required_version    =   "~> 0.13.0"
+    # required_providers {
+    #     aws         =   "~> 2.53.0"
+    #     null        =   "2.1.2"
+    #     local       =   "1.4.0"
+    #     template    =   "2.1.2"
+    #     random      =   "2.2.1"
+    }
+}
