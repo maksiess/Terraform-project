a781b154fa8d45bf90d2d60bf3c340265b5582d3
Murodbey <abdullayevmurod@gmail.com>
2020-12-03 11:29:25 -0600

    updated

--- AWS/RDS/rds.tf
+++ AWS/RDS/rds.tf
@@ -1,17 +1,17 @@
 resource "aws_db_instance" "rds" {
-	identifier = "postgresql"
-	allocated_storage = 20
-	storage_type = "gp2"
-	engine = "postgresql"
-	engine_version = "12.4"
-	instance_class = "db.t2.micro"
-	name = "postgresssql"
-	username = "postgress"
-	password = "foobarbaz"
+	identifier = "${var.identifier}"
+	allocated_storage = "${var.storage_cap}"
+	storage_type = "${var.storage_type}"
+	engine = "${var.engine}"
+	engine_version = "${var.engine_version}"
+	instance_class = "${var.db_instance_type}"
+	name = "${var.name}"
+	username = "${var.username}"
+	password = "${var.password}"
 	parameter_group_name = "default:postgres-12"
-    availability_zone = "us-east-2"
 	publicly_accessible = true
+    skip_final_snapshot = true
 	# db_subnet_group_name = "${aws_db_subnet_group.db.name}"
-	skip_final_snapshot = true #used to delete the repo in the future without this you cant delete. There are bugs reported 
 	# vpc_security_group_ids = ["${aws_security_group.db.id}"]
+    # availability_zone = "us-east-2"
 }
---
+++ AWS/RDS/variable.tf
@@ -0,0 +1,14 @@
+variable "region" {}
+variable "username" {}
+variable "password" {}
+variable "name" {}
+variable "engine_version" {}
+variable "engine" {}
+variable "db-instance_type" {}
+variable "storage_type" {}
+variable "storage_cap" {}
+variable "identifier" {}
+
+
+
+
