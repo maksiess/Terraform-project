23702db860f6544287750116e831c0cfeef797fd
Murodbey <abdullayevmurod@gmail.com>
2020-12-03 11:11:49 -0600

    fixed

--- AWS/RDS/rds.tf
+++ AWS/RDS/rds.tf
@@ -11,7 +11,7 @@ resource "aws_db_instance" "rds" {
 	parameter_group_name = "default:postgres-12"
     availability_zone = "us-east-2"
 	publicly_accessible = true
-	db_subnet_group_name = "${aws_db_subnet_group.db.name}"
+	# db_subnet_group_name = "${aws_db_subnet_group.db.name}"
 	skip_final_snapshot = true #used to delete the repo in the future without this you cant delete. There are bugs reported 
-	vpc_security_group_ids = ["${aws_security_group.db.id}"]
+	# vpc_security_group_ids = ["${aws_security_group.db.id}"]
 }
