2091360d51b411be0c894e7abbd80560174a2f6e
Murodbey <abdullayevmurod@gmail.com>
2020-12-03 11:38:46 -0600

    varibales changed

--- AWS/RDS/rds.tf
+++ AWS/RDS/rds.tf
@@ -1,13 +1,13 @@
 resource "aws_db_instance" "rds" {
-	identifier = "${var.identifier}"
-	allocated_storage = "${var.storage_cap}"
-	storage_type = "${var.storage_type}"
-	engine = "${var.engine}"
-	engine_version = "${var.engine_version}"
-	instance_class = "${var.db_instance_type}"
-	name = "${var.name}"
-	username = "${var.username}"
-	password = "${var.password}"
+	identifier = var.identifier
+	allocated_storage = var.storage_cap
+	storage_type = var.storage_type
+	engine = var.engine
+	engine_version = var.engine_version
+	instance_class = var.db_instance_type
+	name = var.name
+	username = var.username
+	password = var.password
 	parameter_group_name = "default:postgres-12"
 	publicly_accessible = true
     skip_final_snapshot = true
