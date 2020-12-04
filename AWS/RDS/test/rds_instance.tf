846560bb454618fa74329061478f5a90d3526b4f
Murodbey <abdullayevmurod@gmail.com>
2020-12-04 12:42:37 -0600

    updated

--- AWS/RDS/test/rds_instance.tf
+++ AWS/RDS/test/rds_instance.tf
@@ -1,12 +1,12 @@
 data "aws_rds_cluster" "database" {
-  db_instance_identifier = "aurora-cluster"
+  cluster_identifier = "aurora-cluster"
 }
 
 resource "aws_rds_cluster_instance" "cluster_instances" {
   count              = 1
   identifier         = "aurora-cluster-4"
-  cluster_identifier = data.aws_db_instance.database.db_instance_identifier
+  cluster_identifier = data.aws_rds_cluster.database.cluster_identifier
   instance_class     = "db.r4.large"
-  engine             = data.aws_db_instance.database.engine
+  engine             = data.aws_rds_cluster.database.engine
   engine_version     = 11.7
 }
