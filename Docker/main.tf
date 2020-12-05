8ae5d5cc9beffca1761f7703da61ed05a938b682
Murodbey <abdullayevmurod@gmail.com>
2020-12-04 18:22:16 -0600

    updated

--- Docker/main.tf
+++ Docker/main.tf
@@ -7,7 +7,7 @@ resource "null_resource" "test" {
 
   connection {
     user = "ec2-user"
-    # private_key="${file("/home/ec2-user/.ssh/id_rsa")}"
+    private_key="${file("/root/.ssh/id_rsa")}"
     host = "18.207.234.107"
     # agent = true
     timeout = "3m"
