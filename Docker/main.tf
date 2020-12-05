33dd5aa42f8917dc11d775bb1831d8ddae4330ef
Murodbey <abdullayevmurod@gmail.com>
2020-12-04 18:01:06 -0600

    docker_vault

---
+++ Docker/main.tf
@@ -0,0 +1,19 @@
+  data "aws_instances" "test" {
+    instance_tags = {
+        Role = "vault"
+  }
+  provisioner "remote-exec" {
+    inline = [
+        "sudo chmod +x /home/ec2-user/Terraform-project/Docker/vault_docker_compose/docker-install.sh",
+        "sudo . /home/ec2-user/Terraform-project/Docker/vault_docker_compose/docker-install.sh",
+        "sudo docker-compose /home/ec2-user/Terraform-project/Docker/vault_docker_compose/docker-compose.yml up -d"
+    ]
+
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      host = "18.207.234.107"
+    #   private_key = file(var.private_key_file)
+    }
+
+  }
