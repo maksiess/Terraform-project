0701a6cf8efa38fc29a56f78a73f8dca0b832dd3
Murodbey <abdullayevmurod@gmail.com>
2020-12-07 15:35:32 -0600

    updated

--- AWS/RDS/rds_cluster/rds_cluster.tf
+++ AWS/RDS/rds_cluster/rds_cluster.tf
@@ -5,7 +5,7 @@ data "aws_db_snapshot" "database-1_snapshot" {
 
 resource "aws_rds_cluster" "postgresql" {
   cluster_identifier      = var.identifier
-  snapshot_identifier     = data.aws_db_snapshot.database-1_snapshot.id
+  snapshot_identifier     = data.aws_db_snapshot.database-1_snapshot
   # db_snapshot_arn         = var.snapshot_identifier
   engine                  = var.engine
   engine_version          = var.engine_version
