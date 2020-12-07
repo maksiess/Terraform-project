123bd136c49e4d0eb8ceafe7341f16bf0688f07f
Murodbey <abdullayevmurod@gmail.com>
2020-12-07 14:16:51 -0600

    updated

--- AWS/RDS/rds_cluster/rds_cluster.tf
+++ AWS/RDS/rds_cluster/rds_cluster.tf
@@ -1,12 +1,12 @@
-# data "aws_db_snapshot" "database-1_snapshot" {
-#   db_instance_identifier = "database-1"
-#   most_recent            = true
-# }
+data "aws_db_snapshot" "database-1_snapshot" {
+  db_instance_identifier = "database-1"
+  most_recent            = true
+}
 
 resource "aws_rds_cluster" "postgresql" {
   cluster_identifier      = var.identifier
   # db_snapshot_arn         = var.snapshot_identifier
-  # snapshot_identifier     = data.aws_db_snapshot.database-1_snapshot.id
+  snapshot_identifier     = data.aws_db_snapshot.database-1_snapshot.id
   engine                  = var.engine
   engine_version          = var.engine_version
   database_name           = var.name
@@ -22,8 +22,13 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
   count              = var.number
   identifier         = "aurora-cluster-${count.index}"
   cluster_identifier = aws_rds_cluster.postgresql.id
-  db_snapshot_arn    = var.snapshot_identifier
   instance_class     = "db.r4.large"
   engine             = aws_rds_cluster.postgresql.engine
   engine_version     = aws_rds_cluster.postgresql.engine_version
+
+  lifecycle {
+    ignore_changes = [
+      "snapshot_identifier",
+    ]
+}
 }
