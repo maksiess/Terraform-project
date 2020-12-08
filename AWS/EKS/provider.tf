6e74bc81fd6c4440963f4930880e78fbe67da09f
Murodbey <abdullayevmurod@gmail.com>
2020-12-08 12:28:54 -0600

    updated

--- AWS/EKS/provider.tf
+++ AWS/EKS/provider.tf
@@ -2,13 +2,13 @@ provider "aws" {
 region = "${var.region}"
 }
 
-terraform {
-    required_version    =   "~> 0.13.0"
-    required_providers {
-        aws         =   "~> 2.53.0"
-        null        =   "2.1.2"
-        local       =   "1.4.0"
-        template    =   "2.1.2"
-        random      =   "2.2.1"
-    }
-}
+# terraform {
+#     required_version    =   "~> 0.13.0"
+#     required_providers {
+#         aws         =   "~> 2.53.0"
+#         null        =   "2.1.2"
+#         local       =   "1.4.0"
+#         template    =   "2.1.2"
+#         random      =   "2.2.1"
+#     }
+# }
